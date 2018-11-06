cadebc39b8da4d3392c7efe9bcaf754794ae92d9
GAzerty <guilherme.t14@gmail.com>
2018-11-06 21:53:17 +0100

    maj du main, pour des tests

--- main_ruche.py
+++ main_ruche.py
@@ -1,9 +1,16 @@
 #Main du projet faso, protection et statistiques de ruches
 
-import ruche
+import ruche, time
 
 #Ici on appele les fonctions de ruche.py 
 
 
-
-
+#ruche.alarme_vol1()
+#ruche.alarme_vol2()
+ruche.initialisation_ecran()
+#ruche.setRGB(255,0,0)
+#time.sleep(2)
+#ruche.setRGB(0,255,0)
+#time.sleep(2)
+#ruche.setRGB(0,0,255)
+ruche.prompt_values(43,30)
